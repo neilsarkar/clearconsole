3b2819877e8d1f46ec9312583e8bfb73b86eb461
Neil Sarkar <neil.r.sarkar@gmail.com>
2020-07-16 11:16:24 -0700

    Initial Commit

---
+++ Editor/Shortcuts.cs
@@ -0,0 +1,17 @@
+﻿// https://forum.unity.com/threads/shortcut-for-clear-console.758525/
+#if UNITY_EDITOR
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.ShortcutManagement;
+
+public static class Shortcuts {
+	[MenuItem("Window/Clear Console %&v")]
+	public static void ClearConsole() {
+		var assembly = Assembly.GetAssembly(typeof(SceneView));
+		var type = assembly.GetType("UnityEditor.LogEntries");
+		var method = type.GetMethod("Clear");
+		method.Invoke(new object(), null);
+	}
+}
+#endif
---
+++ README.md
@@ -0,0 +1,6 @@
+# ClearConsole
+
+Clear Unity console.
+
+Mac `cmd+alt+v` <br />
+Windows `ctrl+alt+v`
